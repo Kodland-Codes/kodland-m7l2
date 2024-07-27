e3d1d9d8acf313069c6a332dd61508bbfa0645d0
Qusai GAZAWY <candotube@gmail.com>
2024-07-27 21:20:39 +0300

    son

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from model import get_class
 import requests
+import random
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -12,29 +14,54 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-def get_duck_image_url():
-    url = 'https://random-d.uk/api/random'
-    res = requests.get(url)
-    data = res.json()
-    return data['url']
 
-
-@bot.command('duck')
-async def duck(ctx):
-    '''The duck command returns the photo of the duck'''
-    print('hello')
-    image_url = get_duck_image_url()
-    await ctx.send(image_url)
+@bot.command()
+async def merhaba(ctx):
+    await ctx.send("Merhaba! Ben bir botum!")
 
 
 @bot.command()
-async def check(ctx):
+async def check(ctx): #Yapay Zeka Modeli
     if ctx.message.attachments:
         for attachment in ctx.message.attachments:
-            await attachment.save(f"./{attachment.filename}")
-            await ctx.send(get_class(model_path="keras_model.h5", labels_path="labels.txt", image_path=f"./{attachment.filename}"))
+            await attachment.save(f"./images/{attachment.filename}")
+            await ctx.send(get_class(model_path="model1.h5", labels_path="labels1.txt", image_path=f"./images/{attachment.filename}"))
     else:
         await ctx.send("You forgot to upload the image :(")
 
+@bot.command()
+async def sinif_arkadaslarim(ctx): #Yapay Zeka Modeli
+    if ctx.message.attachments:
+        for attachment in ctx.message.attachments:
+            await attachment.save(f"./images/{attachment.filename}")
+            await ctx.send(get_class(model_path="model2.h5", labels_path="labels2.txt", image_path=f"./images/{attachment.filename}"))
+    else:
+        await ctx.send("You forgot to upload the image :(")
+
+
+@bot.command()
+async def rastgele_film(ctx):
+    filmler = ["x", "y", "z"]
+    await ctx.send(random.choice(filmler))
+
+
+@bot.command() #Rastgele resim gonderir
+async def rastgele_resim(ctx):
+    resimler_listesi = os.listdir('resimler') #['resim1.jpeg', 'resim2.jpeg', 'resim3.jpeg']
+    with open(f'resimler/{random.choice(resimler_listesi)}', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
+
+
+@bot.command() #Rastgele resim gonderir
+async def rastgele_gif(ctx):
+    resimler_listesi = os.listdir('gifler') #['resim1.jpeg', 'resim2.jpeg', 'resim3.jpeg']
+    with open(f'gifler/{random.choice(resimler_listesi)}', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
 
 bot.run('TOKEN')
